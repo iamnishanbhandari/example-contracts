c9d8b7b83d3235d282d1666f1d19ac4bda8cfdef
Nishan Bhandari <90060713+iamnishanbhandari@users.noreply.github.com>
2022-09-22 11:49:53 +0545

    Update vote.sol
    completed

--- to-be-completed/vote.sol
+++ to-be-completed/vote.sol
@@ -1,40 +1,57 @@
-pragma solidity ^0.6.4;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
 
 contract cityPoll {
-    
-    struct City {
+    address public owner;
+   
+     struct City {
         string cityName;
         uint256 vote;
         //you can add city details if you want
     }
 
 
-    mapping() public cities; //mapping city Id with the City ctruct - cityId should be uint256
-    mapping() hasVoted; //mapping to check if the address/account has voted or not
+    mapping(uint => City) public cities; //mapping city Id with the City ctruct - cityId should be uint256
+    mapping(address => bool) hasVoted; //mapping to check if the address/account has voted or not
 
-    address owner;
-    uint256 public cityCount = 0; // number of city added
-    constructor() public {
     
-    //TODO set contract caller as owner
-    //TODO set some intitial cities.
+    uint256 public cityCount = 1; // number of city added
+   
+    constructor(string memory _cityName) public {
+    owner=msg.sender;
+    cities[0].cityName=_cityName;
+    cities[0].vote=0;
+  
     }
  
  
-    function addCity() public {
-      //  TODO: add city to the CityStruct
+
+
+    function addCity(string memory _cityName) public {
+         require(owner==msg.sender,"Must be an owner");
+        // City storage newCity = cities[cityCount];
+        cities[cityCount].cityName = _cityName;
+        cityCount++;
 
     }
     
-    function vote() public {
+    function vote(uint8 _cityId ) public {
+        require(!hasVoted[msg.sender],"You have already voted");
+        City storage VoteCity = cities[_cityId];
+         VoteCity.vote++;
+         hasVoted[msg.sender]=true;
+
         
         //TODO Vote the selected city through cityID
 
     }
-    function getCity() public view returns (string memory) {
-     // TODO get the city details through cityID
+    function getCity(uint _cityId) public view returns (string memory) {
+        return cities[_cityId].cityName;
+ 
     }
-    function getVote() public view returns (uint256) {
-    // TODO get the vote of the city with its ID
+    function getVote(uint _cityId) public view returns (uint256) {
+   
+        return cities[_cityId].vote;
     }
 }
